0e8cf55483dbe57ac251211859688ea6b7f81252
Stepa <kardash-99@list.ru>
2024-06-19 10:04:01 +0300

    1. New Structure

---
+++ src/main/java/com/stepashka/App.java
@@ -0,0 +1,13 @@
+package com.stepashka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
